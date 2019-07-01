29e019e496e88bb1c783afa12c2011105b432c84
Ben Drucker <bvdrucker@gmail.com>
2019-06-30 20:52:13 -0700

    add tests for validity, duplicates

--- test.js
+++ test.js
@@ -1,9 +1,30 @@
 'use strict'
 
-var test = require('tape')
-var httpStatusEmojis = require('./')
+const test = require('tape')
+const duplicates = require('array-duplicates')
+const httpStatusEmojis = require('./')
 
-test(function (t) {
-  t.ok(httpStatusEmojis)
+test('all properties are strings', function (t) {
+  t.ok(Object.values(httpStatusEmojis).every(validCodePoint), 'all code points are valid (> 5000)')
+  for (let [key, value] of Object.entries(httpStatusEmojis)) {
+    if (!validCodePoint(value)) {
+      t.fail(`expected emoji, found "${value}" at "${key}"`)
+    }
+  }
   t.end()
 })
+
+test('all values are unique', function (t) {
+  const d = duplicates(Object.values(httpStatusEmojis))
+  const has = d.length
+
+  t.notOk(has, 'should not have duplicates')
+  d.forEach(value => t.fail(`duplicate: ${value}`))
+
+  t.end()
+})
+
+function validCodePoint (value) {
+  // arbitrary, helps catch obvious mistakes
+  return value.codePointAt(0) > 5000
+}
